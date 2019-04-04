e46ece624b25e83053c44f6a5a2feea4e05df153
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2019-04-04 17:24:17 -0300

    Continuação do lab1

--- ri_lab_01/spiders/brasil_elpais.py
+++ ri_lab_01/spiders/brasil_elpais.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pdb
 import scrapy
 import json
 
@@ -17,15 +18,33 @@ class BrasilElpaisSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+    def new(self, response):
+        # pdb.set_trace()
+        yield{
+                'title': response.css('h1.articulo-titulo::text').get(),
+                'subTitle': response.css('h2::text').get(),
+                'author': response.css('span.autor-nombre a::text').get(),
+                'date': response.css('time::attr(datetime)').get(),
+                'url': response.url, 
+                'section': response.css('span.miga.miga_seccion a span::text').get(),
+                'text': response.css('div.articulo-cuerpo p::text').get()
+            }
+
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        
+
+        for articulo in response.css('div.articulo__interior '):
+            h2 = articulo.css('h2')[0]
+            a = h2.css('a')[0]
+            # uri = response.urljoin(a.attrib['href'])
+
+            yield response.follow(a.attrib['href'], callback = self.new)
+
+
+      
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s' % filename)
+       
